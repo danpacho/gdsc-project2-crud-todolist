152d4b3eaadc184e96dbd903c7ca4345816bbae6
Danpa_cho <danpa725@cau.ac.kr>
2022-11-11 15:04:06 +0900

    :sparkles: feat: add main app root component

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import style from "./app.module.css"
+import { TodoListHead, TodoListRender } from "./components/todo"
+import { component, html } from "./core"
+
+const App = () =>
+    component(
+        () => html`
+            <div class="${style.container}">
+                <div id="todo-list-head" class="${style.headContainer}"></div>
+                <div id="todo-list-render" class="${style.listContainer}"></div>
+            </div>
+        `,
+        {
+            isStatic: true,
+        }
+    )
+App().staticRender()
+
+TodoListHead().render("todo-list-head")
+TodoListRender().render("todo-list-render")
